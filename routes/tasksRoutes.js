7480cbad86924f59c4d9a1b8b3757eae1c91ed15
Yasir284 <lambawalay@gmail.com>
2022-12-13 18:43:12 +0530

    added search tasks controller in task controllers

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -7,6 +7,7 @@ const {
   getTodoById,
   deleteTask,
   updateTask,
+  searchTasks,
 } = require("../controller/tasksControllers");
 
 // Routes for Tasks
@@ -14,5 +15,6 @@ router.put("/createTask/:todoId", userAuth, createTask);
 router.get("/getTodoById/:todoId", userAuth, getTodoById);
 router.delete("/deleteTask/:todoId/:taskId", userAuth, deleteTask);
 router.put("/updateTask/:taskId", userAuth, updateTask);
+router.post("/searchTasks/:todoId", userAuth, searchTasks);
 
 module.exports = router;
