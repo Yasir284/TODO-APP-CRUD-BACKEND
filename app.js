f83ba07cfe5c7658710457a04bee6601b450724b
Yasir284 <lambawalay@gmail.com>
2022-12-22 20:11:27 +0530

    added deployed client site link to cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const userRoutes = require("./routes/userRoutes");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://todo-app-crud.onrender.com"],
     credentials: true,
   })
 );
